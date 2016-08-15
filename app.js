9e152588b96574c90c716fa327e8c0de645804c5
Phuc.Hoang <raymondhoang@Raymonds-MacBook-Pro.local>
2016-08-15 09:44:56 +0700

    add webhooks

--- app.js
+++ app.js
@@ -14,8 +14,32 @@ const utils = require('./utils');
 const config = require('./config');
 const sessions = config.sessions;
 
+var app = express();
+
 // Setup bot
-const wit = bot.getWit();
+// const wit = bot.getWit();
 
 // Webserver parameter
 const PORT = process.env.PORT || 8445;
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  console.log(data);
+  res.sendStatus(200);
+});
+
+app.listen(PORT, function() {
+	console.log('Listening on port ' + PORT);
+});
