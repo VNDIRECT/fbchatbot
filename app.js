1e60bf4110b1b05efcb2c7aea73ff364d1aaf648
Phuc.Hoang <raymondhoang@Raymonds-MacBook-Pro.local>
2016-08-27 10:40:20 +0700

    temporarily remove unnecessary codes + add TODOs for future references

--- app.js
+++ app.js
@@ -37,11 +37,6 @@ app.get('/webhook', function(req, res) {
 
 app.post('/webhook', function (req, res) {
 	console.log('/webhook requested');
-	// req.body.entry.forEach(function(pageEntry) {
-	// 	pageEntry.messaging.forEach(function(messaging) {
-	// 		console.log(messaging);
-	// 	});
-	// });
 	fb.processRequest(req, function(message, senderId) { // we got a real message from user
 
 		fb.pretendTyping(senderId); // pretend that the bot is typing...
@@ -91,68 +86,73 @@ app.post('/webhook', function (req, res) {
 							});
 							break;
 
+						//TODO: switch to a formal greeting line.
 						case 'sayHi':
 							fb.sendTextMessage(senderId, `⭐⭐Chào ${user.pronounce} ${user.fbProfile.first_name} ạ!⭐⭐\nEm là Maria Minh Hương, em có thể giúp ${user.pronounce} xem giá chứng khoán, kiểm tra danh mục, đặt lệnh, tư vấn mã chứng khoán cụ thể.\nRất hân hạnh được phục vụ ${user.pronounce}!`);
 							break;
 
-						case 'marketAdvice':
-							var side;
-							if (entities.side) {
-								if (entities.side[0].value == 'buy') {
-									side = 'mua';
-								} else if (entities.side[0].value == 'sell') {
-									side = 'bán';
-								}
-								if (entities.side.length == 1) {
-									if (Math.random() < 0.5) {
-										fb.sendTextMessage(senderId, `Có lẽ không nên ${side} mã ${entities.symbol[0].value} lúc này ${user.pronounce} ạ 🙈`);
-									} else {
-										fb.sendTextMessage(senderId, `Chuẩn, nên ${side} con ${entities.symbol[0].value} sớm ${user.pronounce} ạ! 👍`);
-									}
-								} else {
-									fb.sendTextMessage(senderId, `Không nên mua hay bán mã ${entities.symbol[0].value} vào thời điểm này, ${user.pronounce} nên theo dõi thị trường và đưa ra quyết định sau ạ.`);
-								}
-							} else {
-								fb.sendTextMessage(senderId, `Với mã ${entities.symbol[0].value}, em nghĩ ${user.pronounce} nên tin vào trực giác của mình.`);
-							}
-							break;
-						case 'placeOrder':
-							var sideVal = '';
-							var qttyVal = '';
-							var symbolVal = '';
-							var priceVal = '';
-							if (entities.side) {
-								sideVal = entities.side[0].value;
-							}
-							if (entities.qtty) {
-								qttyVal = entities.qtty[0].value;
-							}
-							if (entities.symbol) {
-								symbolVal = entities.symbol[0].value;
-							}
-							if (entities.price) {
-								priceVal = entities.price[0].value;
-							}
-							if (sideVal != '' && qttyVal != '' && symbolVal != '' && priceVal != '') {
-								console.log(sideVal, qttyVal, symbolVal, priceVal);
-								tradeApi.placeOrder(sideVal, qttyVal, symbolVal, priceVal, '0001032425')
-									.then(function(textArray) {
-										var message = ''
-										if (textArray[0] == 'error') {
-											message = `Xin lỗi ${user.pronounce}, đặt lệnh không thành công. Em nhận được lỗi sau đây từ hệ thống: "${textArray[1]}"`
-										} else if (textArray[0] == 'success') {
-											message = textArray[1];
-										}
+						// TODO: fix with real market advice in the future
+						// case 'marketAdvice':
+						// 	var side;
+						// 	if (entities.side) {
+						// 		if (entities.side[0].value == 'buy') {
+						// 			side = 'mua';
+						// 		} else if (entities.side[0].value == 'sell') {
+						// 			side = 'bán';
+						// 		}
+						// 		if (entities.side.length == 1) {
+						// 			if (Math.random() < 0.5) {
+						// 				fb.sendTextMessage(senderId, `Có lẽ không nên ${side} mã ${entities.symbol[0].value} lúc này ${user.pronounce} ạ 🙈`);
+						// 			} else {
+						// 				fb.sendTextMessage(senderId, `Chuẩn, nên ${side} con ${entities.symbol[0].value} sớm ${user.pronounce} ạ! 👍`);
+						// 			}
+						// 		} else {
+						// 			fb.sendTextMessage(senderId, `Không nên mua hay bán mã ${entities.symbol[0].value} vào thời điểm này, ${user.pronounce} nên theo dõi thị trường và đưa ra quyết định sau ạ.`);
+						// 		}
+						// 	} else {
+						// 		fb.sendTextMessage(senderId, `Với mã ${entities.symbol[0].value}, em nghĩ ${user.pronounce} nên tin vào trực giác của mình.`);
+						// 	}
+						// 	break;
 
-										fb.sendTextMessage(senderId, message);
-									});
-							} else {
-								fb.sendTextMessage(senderId, `Có vẻ ${user.pronounce} đang muốn đặt lệnh phải không ạ? Mời ${user.pronounce} đặt lại lệnh với đầy đủ thông tin: mua/bán, khối lượng, giá, tên mã.`);
-							}
-							break;
+						//TODO: temporarily remove until authentication is resolved
+						// case 'placeOrder':
+						// 	var sideVal = '';
+						// 	var qttyVal = '';
+						// 	var symbolVal = '';
+						// 	var priceVal = '';
+						// 	if (entities.side) {
+						// 		sideVal = entities.side[0].value;
+						// 	}
+						// 	if (entities.qtty) {
+						// 		qttyVal = entities.qtty[0].value;
+						// 	}
+						// 	if (entities.symbol) {
+						// 		symbolVal = entities.symbol[0].value;
+						// 	}
+						// 	if (entities.price) {
+						// 		priceVal = entities.price[0].value;
+						// 	}
+						// 	if (sideVal != '' && qttyVal != '' && symbolVal != '' && priceVal != '') {
+						// 		console.log(sideVal, qttyVal, symbolVal, priceVal);
+						// 		tradeApi.placeOrder(sideVal, qttyVal, symbolVal, priceVal, '0001032425')
+						// 			.then(function(textArray) {
+						// 				var message = ''
+						// 				if (textArray[0] == 'error') {
+						// 					message = `Xin lỗi ${user.pronounce}, đặt lệnh không thành công. Em nhận được lỗi sau đây từ hệ thống: "${textArray[1]}"`
+						// 				} else if (textArray[0] == 'success') {
+						// 					message = textArray[1];
+						// 				}
+
+						// 				fb.sendTextMessage(senderId, message);
+						// 			});
+						// 	} else {
+						// 		fb.sendTextMessage(senderId, `Có vẻ ${user.pronounce} đang muốn đặt lệnh phải không ạ? Mời ${user.pronounce} đặt lại lệnh với đầy đủ thông tin: mua/bán, khối lượng, giá, tên mã.`);
+						// 	}
+						// 	break;
 
 						default:
 							fb.sendTextMessage(senderId, `Xin lỗi, em hiểu yêu cầu của ${user.pronounce}, nhưng em không biết phải làm gì.`);
+							break;
 					}
 				}
 			}).catch(function() {
